e92d6a47a1ccce25c9da6356730666d4a702412d
mashahori <mashahori@users.noreply.github.com>
2018-11-27 10:59:47 +0300

    Исправление ошибок

--- js/setup.js
+++ js/setup.js
@@ -10,41 +10,31 @@ var wizardNames = ['Иван', 'Хуан', 'Себастьян', 'Мария', '
 var wizardSurnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var wizardCoats = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var wizardEyes = ['black', 'red', 'blue', 'yellow', 'green'];
-
+var wizards = [0, 1, 2, 3];
 
 var random = function ( wizardProperty) {
-    return wizardProperty[Math.floor(Math.random() * wizarProperty.length)];
+    return wizardProperty[Math.floor(Math.random() * wizardProperty.length)];
 };
 
-var wizards = [
-  {
-  name: wizardNames[Math.floor(Math.random() * wizardSurnames.length)] + ' ' + wizardSurnames[Math.floor(Math.random() * wizardSurnames.length)],
-  coatColor: wizardCoats[Math.floor(Math.random() * wizardCoats.length)],
-  eyesColor: wizardEyes[Math.floor(Math.random() * wizardEyes.length)]
-  },
-  {
-  name: wizardNames[Math.floor(Math.random() * wizardNames.length)]+ ' ' + wizardSurnames[Math.floor(Math.random() * wizardSurnames.length)],
-  coatColor: wizardCoats[Math.floor(Math.random() * wizardCoats.length)],
-  eyesColor: wizardEyes[Math.floor(Math.random() * wizardEyes.length)]
-  },
-  {
-  name: wizardNames[Math.floor(Math.random() * wizardNames.length)]+ ' ' + wizardSurnames[Math.floor(Math.random() * wizardSurnames.length)],
-  coatColor: wizardCoats[Math.floor(Math.random() * wizardCoats.length)],
-  eyesColor: wizardEyes[Math.floor(Math.random() * wizardEyes.length)]
-  },
-  {
-  name: wizardNames[Math.floor(Math.random() * wizardNames.length)]+ ' ' + wizardSurnames[Math.floor(Math.random() * wizardSurnames.length)],
-  coatColor: wizardCoats[Math.floor(Math.random() * wizardCoats.length)],
-  eyesColor: wizardEyes[Math.floor(Math.random() * wizardEyes.length)]
+var makeWizards = function (massive) {
+  for (var i = 0; i < massive.length; i++) {
+    massive[i] =  {
+    name: random(wizardNames) + ' ' + random(wizardSurnames),
+    coatColor: random(wizardCoats),
+    eyesColor: random(wizardEyes)
+    }
   }
-];
+ return massive;
+}
 
-var renderWizard = function (wizard) {
+makeWizards(wizards);
+
+var renderWizard = function (wizards) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
 
-  wizardElement.querySelector('.setup-similar-label').textContent = wizards[i].name;
-  wizardElement.querySelector('.wizard-coat').style.fill = wizards[i].coatColor;
-  wizardElement.querySelector('.wizard-eyes').style.fill = wizards[i].eyesColor;
+  wizardElement.querySelector('.setup-similar-label').textContent = wizards.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizards.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizards.eyesColor;
 
   return wizardElement;
 }
@@ -55,4 +45,4 @@ for (var i = 0; i < wizards.length; i++) {
   fragment.appendChild(renderWizard(wizards[i]));
 }
 
-  similarListElement.appendChild(fragment);
+similarListElement.appendChild(fragment);
